fe595be59b7861847283d87ff1b97bb3c65aa161
Eric Constantinides <eric@flickerbox.com>
2015-07-09 02:52:33 -0700

    Add immediately invoked function expression to unclutter the global namespace

--- touch-hover.js
+++ touch-hover.js
@@ -20,35 +20,37 @@
 //
 // to do: must disable the close-touch event when inside the exposed area
 //------------------------------------------------------------------------------
-function touchHoverClear() {
-	// only run the touchHoverClear after all the if statements have been run.
-	// otherwise, you'll be clearing before you get answers. :)
-	$('.js-touch-hover').removeClass('js-touch-hover--is-active');
-	$('[data-js-touch-hover--target-name]').removeClass('js-touch-hover--is-active');
-}
-function touchHover(event,eventType) {
-	// this is because older IE uses event.srcElement instead of event.target:
-	var target = event.target ? event.target : event.srcElement;
-	if ($(target).closest('.js-touch-hover').length) {
-		var $touchHoverItem = $(target).closest('.js-touch-hover');
-
-		if (eventType === 'click' && $touchHoverItem.hasClass('js-touch-hover--is-active')) {
-			// clear the hover by performing a 'click'
-			if ( $(target).closest('.js-touch-hover--is-active[data-js-touch-hover--target-name]').length === 0) {
-				touchHoverClear();
-			}
-		} else {
-			if ( $(target).closest('.js-touch-hover--is-active[data-js-touch-hover--target-name]').length === 0) {
-				touchHoverClear();
-			}
-			// check to see if there's data to make active:
-			$touchHoverItem.addClass('js-touch-hover--is-active');
-			// make the data-js-touch-hover--target-recipient "hover" if available
-			$('[data-js-touch-hover--target-name=' + $touchHoverItem.attr('data-js-touch-hover--target-recipient') + ']').addClass('js-touch-hover--is-active');
-		}
-	} else if ( $(target).closest('.js-touch-hover--is-active[data-js-touch-hover--target-name]').length === 0) {
-		touchHoverClear();
+(function($) {
+	function touchHoverClear() {
+		// only run the touchHoverClear after all the if statements have been run.
+		// otherwise, you'll be clearing before you get answers. :)
+		$('.js-touch-hover').removeClass('js-touch-hover--is-active');
+		$('[data-js-touch-hover--target-name]').removeClass('js-touch-hover--is-active');
 	}
-}
-$('html.touch').on('click', function(event){ touchHover(event,'click'); });
-$('html.no-touch').on('mouseover', function(event){ touchHover(event,'hover'); });
+	function touchHover(event,eventType) {
+		// this is because older IE uses event.srcElement instead of event.target:
+		var target = event.target ? event.target : event.srcElement;
+		if ($(target).closest('.js-touch-hover').length) {
+			var $touchHoverItem = $(target).closest('.js-touch-hover');
+
+			if (eventType === 'click' && $touchHoverItem.hasClass('js-touch-hover--is-active')) {
+				// clear the hover by performing a 'click'
+				if ( $(target).closest('.js-touch-hover--is-active[data-js-touch-hover--target-name]').length === 0) {
+					touchHoverClear();
+				}
+			} else {
+				if ( $(target).closest('.js-touch-hover--is-active[data-js-touch-hover--target-name]').length === 0) {
+					touchHoverClear();
+				}
+				// check to see if there's data to make active:
+				$touchHoverItem.addClass('js-touch-hover--is-active');
+				// make the data-js-touch-hover--target-recipient "hover" if available
+				$('[data-js-touch-hover--target-name=' + $touchHoverItem.attr('data-js-touch-hover--target-recipient') + ']').addClass('js-touch-hover--is-active');
+			}
+		} else if ( $(target).closest('.js-touch-hover--is-active[data-js-touch-hover--target-name]').length === 0) {
+			touchHoverClear();
+		}
+	}
+	$('html.touch').on('click', function(event){ touchHover(event,'click'); });
+	$('html.no-touch').on('mouseover', function(event){ touchHover(event,'hover'); });
+})(jQuery);
